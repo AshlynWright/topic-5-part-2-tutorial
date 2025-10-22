577028d8966aaaaba03872a0d5b2afe0c51da9a0
Ashlyn Wright <362581399@GoTVDSB.ca>
2025-10-22 08:44:38 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,102 @@
+﻿namespace topic_5_part_2_tutorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int grade;
+            //Console.WriteLine("What was your grade?");
+            //int.TryParse(Console.ReadLine(), out grade);
+
+            //if (grade >= 50)
+            //{
+            //    Console.WriteLine("You passed!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("You failed.");
+            //}
+
+
+
+            //Console.WriteLine("What was your grade?");
+            //int.TryParse(Console.ReadLine(), out grade);
+
+            //if (grade < 50)
+            //{
+            //    Console.WriteLine("That is an F!");
+            //}
+            //else if (grade <= 65)
+            //{
+            //    Console.WriteLine("That is a D!");
+            //}
+            //else if (grade <= 75)
+            //{
+            //    Console.WriteLine("That is a C!");
+            //}
+            //else if (grade <= 85)
+            //{
+            //    Console.WriteLine("That is a B!");
+            //}
+            //else if (grade > 85)
+            //{
+            //    Console.WriteLine("That is a A!");
+            //}
+
+
+            //if (grade >= 50)
+            //    Console.WriteLine("That is an F!");
+            //else if (grade >= 65)
+            //    Console.WriteLine("That is a D!");
+            //else if (grade >= 75)
+            //    Console.WriteLine("That is a C!");
+            //else if (grade >= 85)
+            //    Console.WriteLine("That is a B!");
+            //else
+            //    Console.WriteLine("That is a A!");
+
+
+            Console.WriteLine("what is 4 + 4?");
+             
+            
+            if (Console.ReadLine() == "A")
+            {
+                Console.WriteLine("Correct!");
+            }
+            else if (Console.ReadLine() == "B")
+            {
+                Console.WriteLine("wrong!");
+            }
+            else if (Console.ReadLine() == "C")
+            {
+                Console.WriteLine("wrong!");
+            }
+            else if (Console.ReadLine() == "D")
+            {
+                Console.WriteLine("wrong!");
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
